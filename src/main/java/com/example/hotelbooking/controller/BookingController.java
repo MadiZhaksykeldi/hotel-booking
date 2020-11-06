9395d68f77abafaf4679b733f1bfd3f8bc577d5e
Madi <madizhaksigeldi@gmail.com>
2020-11-06 23:24:03 +0600

    Hystrix + Bulkhead

--- src/main/java/com/example/hotelbooking/controller/BookingController.java
+++ src/main/java/com/example/hotelbooking/controller/BookingController.java
@@ -3,11 +3,14 @@ package com.example.hotelbooking.controller;
 import com.example.hotelbooking.model.Booking;
 import com.example.hotelbooking.service.BookingService;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -21,8 +24,20 @@ public class BookingController {
         return service.getAllBooking();
     }
 
+    @HystrixCommand(fallbackMethod = "getFallbackBooking",
+            threadPoolProperties = {
+                    @HystrixProperty(name = "coreSize", value = "20"),
+                    @HystrixProperty(name = "maxQueueSize", value = "10"),
+            }
+    )
     @GetMapping(path = "/bookings/{id}")
     public Optional<Booking> getBookingById(@PathVariable("id") long id) {
         return service.getBookingById(id);
+    }
+
+    public List<Booking> getFallbackBooking(@PathVariable("id") long id){
+        List<Booking> bookingList = new ArrayList<>();
+        bookingList.add(new Booking());
+        return bookingList;
     }
 }
